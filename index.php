65b4d1119a28747860a4d45fcb1e32125005ceb4
Soggywaffles27 <99052580+Soggywaffles27@users.noreply.github.com>
2022-09-06 08:35:38 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
-<?PHP
-echo shell_exec("python test.py 'parameter1'");
-?>
+<html>
+<head>
+  <title>Run my Python files</title>
+  <?PHP
+  echo shell_exec("python test.py 'parameter1'");
+  ?>
+</head>
+
+  
