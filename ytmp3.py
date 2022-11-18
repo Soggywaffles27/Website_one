3f60cc4bd3bbedb004577dade297e7a646091a38
Soggywaffles27 <99052580+Soggywaffles27@users.noreply.github.com>
2022-11-18 14:43:46 -0500

    Update ytmp3.py

--- ytmp3.py
+++ ytmp3.py
@@ -9,10 +9,8 @@ def mp3():
   yt = pytube.YouTube("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
 
   vids= yt.streams.all()
-  for i in range(len(vids)):
-      print(i,'. ',vids[i])
 
-  vnum = int(input("Enter vid num: "))
+  vnum = 0
 
   parent_dir = r"C:\YTDownloads"
   vids[vnum].download(parent_dir)
