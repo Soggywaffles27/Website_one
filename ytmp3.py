74b0269051d26b6099057ec00f46eee515f0469f
Soggywaffles27 <99052580+Soggywaffles27@users.noreply.github.com>
2022-11-18 12:32:03 -0500

    Update ytmp3.py

--- ytmp3.py
+++ ytmp3.py
@@ -1,18 +1,28 @@
 #https://www.youtube.com/watch?v=dQw4w9WgXcQ
 def mp3():
-  import youtube_dl
-  def download_ytvid_as_mp3():
-      video_url = input("enter url of youtube video:")
-      video_info = youtube_dl.YoutubeDL().extract_info(url = video_url,download=False)
-      filename = f"{video_info['title']}.mp3"
-      options={
-          'format':'bestaudio/best',
-          'keepvideo':False,
-          'outtmpl':filename,
-      }
+  import os
+  import subprocess
 
-      with youtube_dl.YoutubeDL(options) as ydl:
-          ydl.download([video_info['webpage_url']])
+  import pytube
 
-      print("Download complete... {}".format(filename))
-  download_ytvid_as_mp3()
+  yt = pytube.YouTube("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+
+  vids= yt.streams.all()
+  for i in range(len(vids)):
+      print(i,'. ',vids[i])
+
+  vnum = int(input("Enter vid num: "))
+
+  parent_dir = r"C:\YTDownloads"
+  vids[vnum].download(parent_dir)
+
+  new_filename = input("Enter filename (including extension): "))  # e.g. new_filename.mp3
+
+  default_filename = vids[vnum].default_filename  # get default name using pytube API
+  subprocess.run([
+      'ffmpeg',
+      '-i', os.path.join(parent_dir, default_filename),
+      os.path.join(parent_dir, new_filename)
+  ])
+
+  print('done')
