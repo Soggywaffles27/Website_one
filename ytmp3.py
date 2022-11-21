73c29d04cff34e4b96eb1d18c52d37b7a5e88c9a
Soggywaffles27 <99052580+Soggywaffles27@users.noreply.github.com>
2022-11-21 09:03:05 -0500

    Update ytmp3.py

--- ytmp3.py
+++ ytmp3.py
@@ -7,22 +7,24 @@ def mp3():
   import pytube
 
   yt = pytube.YouTube("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
-
+  
+  stream = yt.streams.first()
+  stream.download()
   #vids= yt.streams.all()
 
-  vnum = 0
+#   vnum = 0
 
-  parent_dir = r"C:\YTDownloads"
-  #vids[vnum].download(parent_dir)
-  yt.download(parent_dir)
+#   parent_dir = r"C:\YTDownloads"
+#   #vids[vnum].download(parent_dir)
+#   yt.download(parent_dir)
   
-  new_filename = "pls work"
+#   new_filename = "pls work"
 
-  default_filename = vids[vnum].default_filename  # get default name using pytube API
-  subprocess.run([
-      'ffmpeg',
-      '-i', os.path.join(parent_dir, default_filename),
-      os.path.join(parent_dir, new_filename)
-  ])
+#   default_filename = vids[vnum].default_filename  # get default name using pytube API
+#   subprocess.run([
+#       'ffmpeg',
+#       '-i', os.path.join(parent_dir, default_filename),
+#       os.path.join(parent_dir, new_filename)
+#   ])
 
-  print('done')
+#   print('done')
